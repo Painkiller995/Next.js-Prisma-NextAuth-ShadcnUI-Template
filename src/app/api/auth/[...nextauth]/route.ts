854c9075f26fa3fa2b9c1f93230c7aa507be26aa
Painkiller995 <fahdakramdaher@gmail.com>
2024-02-20 22:21:23 +0100

    Initial

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,82 @@
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import prisma from '@@/prisma/client';
+import { User } from '@prisma/client';
+import { compare } from 'bcrypt';
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'hello@example.com',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        // This is no credentials provided
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        // Look up the user in the database
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        // Can't find user
+        if (!user) {
+          return null;
+        }
+
+        // Check password
+        // Using bcrypt to compare the passwords
+        const isPasswordValid = await compare(credentials.password, user.password);
+
+        // Password is invalid
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        // Return the user object
+        return {
+          id: `${user.id}`,
+          email: user.email,
+          name: user.name,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    // Add user id to session
+    // This helps look up the data in the database based on user id
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.id,
+      },
+    }),
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as User;
+        return {
+          ...token,
+          id: u.id,
+        };
+      }
+      return token;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,24 @@
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@@/prisma/client';
+import { prismaExclude } from '@@/prisma/prisma-exclude';
+import { authOptions } from '../auth/[...nextauth]/route';
+
+export async function GET(request: NextRequest) {
+  try {
+    // Check if user is authenticated
+    const session = await getServerSession(authOptions);
+    if (!session) return NextResponse.json({}, { status: 401 });
+
+    // Get users without sensitive data using prismaExclude function
+    const users = await prisma.user.findMany({
+      orderBy: { name: 'asc' },
+      select: prismaExclude('User', ['password']),
+    });
+
+    // Return users list
+    return NextResponse.json(users, { status: 200 });
+  } catch {
+    return NextResponse.json({ error: 'Invalid request format' }, { status: 400 });
+  }
+}
