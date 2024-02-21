e88190da53d8927ee5c9f3eb4dd7a39127c1744f
Painkiller995 <fahdakramdaher@gmail.com>
2024-02-21 08:47:54 +0100

    add signupSchemaForm

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -6,8 +6,26 @@ export const authSchema = z.object({
 });
 
 export const signupSchema = z.object({
-  firstName: z.string().min(1, 'Name is required.').max(255),
-  lastName: z.string().min(1, 'Name is required.').max(255),
+  firstName: z.string().min(1, 'First name is required.').max(255),
+  lastName: z.string().min(1, 'Last name is required.').max(255),
   email: z.string().email('Invalid email format.').min(1, 'Email is required.').max(255),
   password: z.string().min(4, 'Password must be at least 4 characters.').max(255),
 });
+
+export const signupSchemaForm = z
+  .object({
+    firstName: z.string().min(1, 'Name is required.').max(255),
+    lastName: z.string().min(1, 'Name is required.').max(255),
+    email: z.string().email('Invalid email format.').min(1, 'Email is required.').max(255),
+    confirmEmail: z.string().email('Invalid email format.').min(1, 'Email is required.').max(255),
+    password: z.string().min(4, 'Password must be at least 4 characters.').max(255),
+    confirmPassword: z.string().min(4, 'Password must be at least 4 characters.').max(255),
+  })
+  .refine((data) => data.email === data.confirmEmail, {
+    message: "Emails don't match",
+    path: ['confirmEmail'],
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
